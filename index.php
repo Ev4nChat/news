49c9fb1b8f980ba42a7fc3365ea0d6471f1509fa
Evangelos Chatzistefanou <ev4nchat@gmail.com>
2025-02-01 12:33:01 +0200

    Build endpoint URL

--- index.php
+++ index.php
@@ -6,3 +6,11 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $apiToken = $_ENV['API_TOKEN'] ?? '';
+
+$baseUrl = "https://api.thenewsapi.com/v1/news/top";
+$queryParams = http_build_query([
+    'locale'    => 'us',
+    'language'  => 'en',
+    'api_token' => $apiToken
+]);
+$url = $baseUrl . '?' . $queryParams;
